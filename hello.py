753eae7a15e411e426fc5465d1e9fa5db8a26907
demo52490 <boys52490@gmail.com>
2025-04-01 10:37:11 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 print("Hello World")
 print("2nd time running jenkins")
+import numpy as np
+
+# Create a 3x3 matrix
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+# Print the matrix
+print("Matrix:")
+print(matrix)
